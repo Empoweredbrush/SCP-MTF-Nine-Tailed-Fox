286dd2c10a7a4746ea809b9f5d08477332657757
Empoweredbrush <Empoweredbrushfour@gmail.com>
2025-03-18 15:25:30 -0600

    Figuring out rust

---
+++ MTF_Game/target/debug/MTF_Game.d
@@ -0,0 +1,1 @@
+/Users/shonashby/Documents/GitHub/SCP-MTF-Nine-Tailed-Fox/MTF_Game/target/debug/MTF_Game: /Users/shonashby/Documents/GitHub/SCP-MTF-Nine-Tailed-Fox/MTF_Game/src/main.rs
---
+++ MTF_Game/target/debug/deps/MTF_Game-0ddca5029810a206.d
@@ -0,0 +1,5 @@
+/Users/shonashby/Documents/GitHub/SCP-MTF-Nine-Tailed-Fox/MTF_Game/target/debug/deps/MTF_Game-0ddca5029810a206: src/main.rs
+
+/Users/shonashby/Documents/GitHub/SCP-MTF-Nine-Tailed-Fox/MTF_Game/target/debug/deps/MTF_Game-0ddca5029810a206.d: src/main.rs
+
+src/main.rs:
