a7119655fcb84440af3f3f68ed3e081a46b188a6
Empoweredbrush <Empoweredbrushfour@gmail.com>
2025-03-20 13:19:14 -0600

    Added code in main to get player's name

--- MTF_Game/src/main.rs
+++ MTF_Game/src/main.rs
@@ -3,9 +3,17 @@ use std::cmp::Ordering;
 use rand::Rng;
 
 fn main() {
-    
+    println!("What is your name?\n");
+
+    let mut name = String::new();
+
+    io::stdin()
+        .read_line(&mut name)
+        .expect("Failed to read line");
+
+    println!("{}", name);
 }
 
 fn start() {
-    
+
 }
